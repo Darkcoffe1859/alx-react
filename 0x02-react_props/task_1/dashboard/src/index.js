4c326cce4794930a0b8eb4624ba58412f97e514a
Darkcoffe1859 <shency49@gmail.com>
2024-01-16 10:54:48 +0100

    react

--- 0x02-react_props/task_1/dashboard/src/index.js
+++ 0x02-react_props/task_1/dashboard/src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Notifications from "./Notifications";
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+const rootNotifications = ReactDOM.createRoot(document.getElementById("root-notifications"));
+rootNotifications.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
+);
+
+reportWebVitals();
--- 0x02-react_props/task_1/dashboard/src/utils/utils.js
+++ 0x02-react_props/task_1/dashboard/src/utils/utils.js
@@ -1,7 +1,3 @@
 export const getFullYear = () => new Date().getFullYear();
 
-export const getFooterCopy = (isIndex) =>
-	isIndex ? 'Holberton School' : 'Holberton School main dashboard';
-
-export const getLatestNotification = () =>
-	'<strong>Urgent Requirement</strong> - complete by EOD';
+export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton School main dashboard");
